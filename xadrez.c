ab67e765858b19abf0812bd8215fcf83cb783bdd
RNL-BATISTA <ronaldinhobatista9@hotmail.com>
2025-05-29 17:00:19 +0000

    Adicionado movimentação do cavalo e otimizações do codigo.

--- xadrez.c
+++ xadrez.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 
 int main (){
-    int torre, bispo = 0, queen;
+    int torre=0, bispo = 0, queen, cavalo1, cavalo2=1;
     //Declarando variaves
-
-
+    printf("Torre\n");
+    //Movimento torre
     while(torre < 5){
         printf("Torre Direita (%d)\n", torre);
         torre++;
-        //Movimento torre
+        
     }
-
+    printf("Bispo\n"); //Espaçamento para melhor entender as respostas.
+   //Movimento bispo
     do{
         printf("Bispo Cima Direta (%d)\n", bispo);
         bispo++;
-        //Movimento bispo
+       
     }while (bispo<5);
-
-    for (queen == 0; queen < 8; queen++)
+    printf("Rainha\n"); //Espaçamento para melhor entender as respostas.
+    //Movimento rainha
+    for (queen = 0; queen < 8; queen++)
     {
         printf("Rainha Esquerda (%d)\n", queen);
-        //Movimento rainha
+        
     }
-    
+    printf("Cavalo\n"); //Espaçamento para melhor entender as respostas.
+    for(cavalo1=1; cavalo1<2; cavalo1++)
+    {
+        while(cavalo2<=2){
+            printf("Baixo\n");
+            cavalo2++;
+        }
+        printf("Esquerda\n");
+    }
 }
